324830f1ec5ca5806f320cc6014ef3a2573ab43f
KerlyNaranjo <k.erly5@hotmail.com>
2016-05-26 08:15:23 -0500

    Probando

---
+++ Ejercicio3.9.py
@@ -0,0 +1,11 @@
+#Mayus Minus y Capital letter
+
+frase = str(input("Ingresa una frase: "))
+
+print (frase.upper())
+print (frase.lower())
+print (frase.capitalize())
+print (frase.strip())
+print (frase.rstrip())
+print (frase.lstrip())
+print (frase.replace(" ",""))
---
+++ Ejercicio4.1.py
@@ -0,0 +1,6 @@
+#spleet line
+#Ingresar una frase y contar el número de palabras
+
+frase = str(input('Ingrese una frase: '))
+
+nombres.split(frase)
